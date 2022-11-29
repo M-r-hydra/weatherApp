f84e8a525925ca5255a3dc672c9c3e77c5e76f82
M-r-hydra <me74242@gmail.com>
2022-11-29 12:17:05 +0330

    add custom Module Types and init Wheather shower component

---
+++ src/Components/WhatherShower/WheatherShower.tsx
@@ -0,0 +1,16 @@
+// React
+import React from "react";
+// React
+// CSS
+import styles from "./WheatherShower.module.css";
+// CSS
+// Models
+
+// Models
+type WheatherShowerProps = {};
+
+const WheatherShower = () => {
+  return <div>WheatherShower</div>;
+};
+
+export default WheatherShower;
