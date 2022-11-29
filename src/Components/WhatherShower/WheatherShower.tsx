adfd035dc2c527664609a38e0f67a9c181dd0bee
M-r-hydra <me74242@gmail.com>
2022-11-30 01:06:22 +0330

    add props to WheatherShower

--- src/Components/WhatherShower/WheatherShower.tsx
+++ src/Components/WhatherShower/WheatherShower.tsx
@@ -13,9 +13,45 @@ type WheatherShowerProps = {
 };
 
 const WheatherShower: React.FunctionComponent<WheatherShowerProps> = ({
-  data,
+  data: {
+    current: {
+      cloud,
+      condition,
+      feelslike_c,
+      feelslike_f,
+      gust_kph,
+      gust_mph,
+      humidity,
+      is_day,
+      last_updated,
+      last_updated_epoch,
+      precip_in,
+      precip_mm,
+      pressure_in,
+      pressure_mb,
+      temp_c,
+      temp_f,
+      uv,
+      vis_km,
+      vis_miles,
+      wind_degree,
+      wind_dir,
+      wind_kph,
+      wind_mph,
+    },
+    location: {
+      country,
+      lat,
+      localtime,
+      localtime_epoch,
+      lon,
+      name,
+      region,
+      tz_id,
+    },
+  },
 }) => {
-  return <div>WheatherShower</div>;
+  return <div></div>;
 };
 
 export default WheatherShower;
