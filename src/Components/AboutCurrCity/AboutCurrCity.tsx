532c6865f070b96d8d13b15c157988ef211ebda6
M-r-hydra <me74242@gmail.com>
2022-11-30 01:14:36 +0330

    install tailwind and aboutCurrCity

---
+++ src/Components/AboutCurrCity/AboutCurrCity.tsx
@@ -0,0 +1,18 @@
+// React
+import React from "react";
+// React
+// CSS
+import styles from "./AboutCurrCity.module.css";
+// CSS
+
+type AboutCurrCityProps = {
+  cityName: string;
+};
+
+const AboutCurrCity: React.FunctionComponent<AboutCurrCityProps> = ({
+  cityName,
+}) => {
+  return <div className=""></div>;
+};
+
+export default AboutCurrCity;
